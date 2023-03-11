194308be7527583327b550e4993010d96bc87719
Thijmen <yeaaccount@gmail.com>
2023-03-11 16:52:09 +0100

    updated headertop for the added view

--- app/components/headerTop.vue
+++ app/components/headerTop.vue
@@ -1,9 +1,15 @@
 <template>
-  <div class="header pl-[11px] w-full flex ">
+  <div class="header  w-full flex pt-[11px]">
+    <div v-if = 'this.variant == "home"' class="pl-[16px]"></div>
     <img @click = "this.$router.go(-1)" v-if="variant == 'back' || variant == 'userTitle'" src="@/assets/icons/back.svg">
-    <img @click = "this.$router.push({name: 'Menu'})" v-if="variant == 'settings'" src="@/assets/icons/settings.svg">
+    <img  v-if="variant == 'settings'" src="@/assets/icons/settings.svg">
 
-    <p class="font-bold pl-[5px] tracking-widest text-[40px]"  > {{ this.title }}</p>
+    <p class="font-bold pl-[5px] tracking-widest text-[30px] uppercase"  > {{ this.title }}</p>
+    <div v-if = 'this.variant == "home"' class="justify-end w-full flex pr-[36px] ">
+      <img @click="this.$emit('clickedOnProfilePicture')" class="h-[39px] w-[39px] rounded-[10px] object-cover pt-[3px]" src="https://i.imgur.com/G0eOXkZ.png">
+
+    </div>
+
   </div>
 </template>
 
@@ -13,28 +19,20 @@ export default {
     variant: {
       type: String,
       validators: value =>
-        ['settings', 'back', 'userTitle'].includes(value),
+        ['settings', 'back', 'userTitle','home'].includes(value),
         default: 'back',
     },
     title: {
       type: String,
-      default: 'SIMPLE-CHAT'
+      default: "simple-chat"
     },
     data() {
       return {
         fontSize: 40,
         yes: true,
         showMenu: true,
-
       }
     },
-    mounted() {
-      if (this.variant == 'userTitle') {
-        this.fontSize = 30;
-
-      }
-      this.fontSize = 40;
-    }
   },
 }
 
