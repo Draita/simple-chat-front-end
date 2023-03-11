eb30c7e186b07d6a682247b5554fe2224805a7f0
Thijmen <yeaaccount@gmail.com>
2023-03-11 16:50:39 +0100

    show the list of firneds pending and send here friend requests

---
+++ app/chat-app/views/Friends.vue
@@ -0,0 +1,22 @@
+<template>
+   <div class="grid  grid-flow-col pl-[34px]  w-full">
+        <input v-model="searchInput" class="inp w-[245px] h-[25px]" placeholder="Enter a username#0000">
+        <img @click="addFriend" class=" mt-[9px] pr-[40px]" src="@/assets/icons/add.svg">
+    </div>
+</template>
+
+<script>
+export default{
+    data(){
+        return {
+            searchInput: "",
+        }
+    },
+    methods:{
+        addfriend(){
+
+        }
+    }
+}
+
+</script>
