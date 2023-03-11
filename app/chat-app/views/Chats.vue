8c498a67fb8ded5b0c52bc3d05269b3f3057956a
Thijmen <yeaaccount@gmail.com>
2023-03-11 16:50:24 +0100

    added chats view for showing all the chats that are anvialable to the user

---
+++ app/chat-app/views/Chats.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="grid  grid-flow-col pl-[34px] w-full">
+        <input v-model="searchInput" class="inp w-[280px] h-[25px]" placeholder="SEARCH FOR CHAT">
+
+    </div>
+    <UsersAndGroupslist />
+</template>
+
+<script>
+import UsersAndGroupslist from "@/components/usersAndGroupsList.vue"
+
+
+
+export default {
+    components: {
+        UsersAndGroupslist
+    },
+    data(){
+        return {
+            searchInput: "",
+        }
+    }
+}
+
+</script>
