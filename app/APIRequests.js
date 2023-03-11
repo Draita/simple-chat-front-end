f3714dfa21c1fdb00952b9b70bf6391926284ee3
Thijmen <yeaaccount@gmail.com>
2023-03-11 16:49:49 +0100

    addded api's friend requests login and register

--- app/APIRequests.js
+++ app/APIRequests.js
@@ -1,22 +1,61 @@
-import { useRouter } from 'vue-router'
-import axios from 'axios';
+import { useRouter } from "vue-router";
+import axios from "axios";
 
-async function register(username,email,password) {
-    try {
-        const response = axios.post("/register", {
-            email,
-            password,
-            username,
-        })
-        return response
-
-      } catch (error) {
-        console.log(error); // Handle error
-        return error
-      }
+async function register(username, email, password) {
+  try {
+    const response = axios.post("/register", {
+      email,
+      password,
+      username,
+    });
+    return response;
+  } catch (error) {
+    console.log(error); // Handle error
+    return error;
+  }
 }
 
+async function login(email, password) {
+  try {
+    const response = axios.post("/login", {
+      email,
+      password,
+    });
+    return response;
+  } catch (error) {
+    console.log(error); // Handle error
+    return error;
+  }
+}
+
+async function getUsersAndGroupsList() {
+  try {
+    const response = axios.get("/friend/requests").catch(function (error) {
+      return error;
+    });
+
+    return response;
+  } catch (error) {
+    console.log(error); // Handle error
+    return error;
+  }
+}
+
+async function addFriend(usernameAndTag) {
+  try {
+    const response = axios.post("/friend/add", {
+      usernameAndTag
+    });
+    return response;
+  } catch (error) {
+    console.log(error); // Handle error
+    return error;
+  }
+}
 
 export default {
-    register,
-}
+  register,
+  login,
+  getUsersAndGroupsList,
+  addFriend,
+};
