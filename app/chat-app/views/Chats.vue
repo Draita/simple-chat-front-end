4672acdee11acb0b314b1737ec2d2fa1e4af1e0b
Thijmen <yeaaccount@gmail.com>
2023-03-15 19:45:21 +0100

    chat lists is loaded

--- app/chat-app/views/Chats.vue
+++ app/chat-app/views/Chats.vue
@@ -3,11 +3,14 @@
         <input v-model="searchInput" class="inp w-[280px] h-[25px]" placeholder="SEARCH FOR CHAT">
 
     </div>
+    <UsersAndGroupslist v-if="!isLoading" :rooms="this.rooms"></UsersAndGroupslist>
+
     <UsersAndGroupslist />
 </template>
 
 <script>
 import UsersAndGroupslist from "@/components/usersAndGroupsList.vue"
+import APIRequests from "@/APIRequests"
 
 
 
@@ -15,11 +18,19 @@ export default {
     components: {
         UsersAndGroupslist
     },
-    data(){
+    data() {
         return {
+            isLoading: true,
             searchInput: "",
+            room: "",
         }
-    }
+    },
+    async created() {
+        const response = await APIRequests.getRoomList()
+        this.friends = response.data
+        this.isLoading = false
+        this.rooms = (response.data)
+    },
 }
 
 </script>
