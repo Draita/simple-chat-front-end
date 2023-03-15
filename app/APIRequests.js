fbb9f202d226b6eec3c5f70d19b464b51b44e703
Thijmen <yeaaccount@gmail.com>
2023-03-15 19:44:43 +0100

    added getUser getRoomList, add Friend

--- app/APIRequests.js
+++ app/APIRequests.js
@@ -1,6 +1,16 @@
 import { useRouter } from "vue-router";
 import axios from "axios";
 
+async function getUser() {
+  try {
+    const response = axios.get("/users/self", {});
+    return response;
+  } catch (error) {
+    console.log(error); // Handle error
+    return error;
+  }
+}
+
 async function register(username, email, password) {
   try {
     const response = axios.post("/register", {
@@ -28,7 +38,7 @@ async function login(email, password) {
   }
 }
 
-async function getUsersAndGroupsList() {
+async function getFriendRequests() {
   try {
     const response = axios.get("/friend/requests").catch(function (error) {
       return error;
@@ -41,10 +51,62 @@ async function getUsersAndGroupsList() {
   }
 }
 
+async function getRoomList() {
+  try {
+    const response = axios.get("/room/list").catch(function (error) {
+      return error;
+    });
+
+    return response;
+  } catch (error) {
+    console.log(error); // Handle error
+    return error;
+  }
+}
+
+async function cancelFriendRequest(addressee) {
+  try {
+    const response = axios.put("/friend/cancel", {
+      addressee,
+    });
+    return response;
+  } catch (error) {
+    console.log(error); // Handle error
+    return error;
+  }
+}
+
+async function acceptFriendRequest(requester) {
+  try {
+    console.log(requester);
+    const response = axios.put("/friend/accept", {
+      requester,
+    });
+    return response;
+  } catch (error) {
+    console.log(error); // Handle error
+    return error;
+  }
+}
+
 async function addFriend(usernameAndTag) {
   try {
     const response = axios.post("/friend/add", {
-      usernameAndTag
+      usernameAndTag,
+    });
+    return response;
+  } catch (error) {
+    console.log(error); // Handle error
+    return error;
+  }
+}
+
+async function saveProfile(data) {
+  try {
+    const response = await axios.post("/users/update_profile", data, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
     });
     return response;
   } catch (error) {
@@ -56,6 +118,11 @@ async function addFriend(usernameAndTag) {
 export default {
   register,
   login,
-  getUsersAndGroupsList,
+  getFriendRequests,
   addFriend,
+  cancelFriendRequest,
+  acceptFriendRequest,
+  getRoomList,
+  getUser,
+  saveProfile,
 };
