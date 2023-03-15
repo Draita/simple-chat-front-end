a25abd3a5ee58764d2516a4baf6a5f00d68e5807
Thijmen <yeaaccount@gmail.com>
2023-03-15 19:45:48 +0100

    added the ability to cancel and accept friend requests

--- app/chat-app/views/Friends.vue
+++ app/chat-app/views/Friends.vue
@@ -1,22 +1,44 @@
 <template>
-   <div class="grid  grid-flow-col pl-[34px]  w-full">
-        <input v-model="searchInput" class="inp w-[245px] h-[25px]" placeholder="Enter a username#0000">
-        <img @click="addFriend" class=" mt-[9px] pr-[40px]" src="@/assets/icons/add.svg">
+    <div class="grid  grid-flow-col pl-[34px] w-full">
+
+        <input v-model="friend" class="inp w-[245px] h-[25px]" placeholder="Enter a username#0000">
+        <img @click="addFriend" class="mb-[-9px] pr-[40px]" src="@/assets/icons/add.svg">
+
     </div>
+    <friendRequestsList v-if="!isLoading" :friends = "this.friends"></friendRequestsList>
 </template>
 
 <script>
-export default{
-    data(){
+
+
+import APIRequests from "@/APIRequests"
+import friendRequestsList from "@/components/friendRequestsList.vue"
+
+
+
+export default {
+    data() {
         return {
-            searchInput: "",
+            friend: "",
+            friends: "",
+            isLoading: true,
         }
     },
-    methods:{
-        addfriend(){
+    async created() {
+        const response = await APIRequests.getFriendRequests()
+        this.friends = response.data
+        this.isLoading = false
 
+    },
+    components: {
+        friendRequestsList
+    },
+    methods: {
+        async addFriend() {
+            const response = await APIRequests.addFriend(this.friend)
+            this.friends.push(response.data)
         }
-    }
+    },
 }
 
 </script>
