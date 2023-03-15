9e6e1b937f502b1b822044ed14f2528b4319c75e
Thijmen <yeaaccount@gmail.com>
2023-03-15 19:44:54 +0100

    data is loaded when chatapp is loaded

--- app/chat-app/ChatApp.vue
+++ app/chat-app/ChatApp.vue
@@ -1,9 +1,46 @@
 <script setup>
+import { userStore } from '@/stores/userSelf'
+
+
+async function setupData() {
+
+}
+setupData()
+
+
+
+</script>
+
+
+<script>
+import { userStore } from '@/stores/userSelf'
+import APIRequests from "@/APIRequests"
+
+
+export default {
+  data() {
+    return {
+      isLoading: true,
+
+    }
+  },
+
+  async created() {
+    const userdata = await APIRequests.getUser()
+    const store = userStore()
+    await store.setUserData(userdata.data)
+
+    this.isLoading = false
+
+  }
+
+
+
+}
 
 </script>
 
 <template>
-
-  <RouterView></RouterView>
+  <RouterView v-if = "!isLoading"></RouterView>
 </template>
 
