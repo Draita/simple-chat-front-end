4b140f3386d93a03c9bfee43512900984bd16d15
Thijmen <yeaaccount@gmail.com>
2023-03-15 19:43:17 +0100

    -

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,11 @@ import vue from '@vitejs/plugin-vue'
 import webfontDownload from 'vite-plugin-webfont-dl';
 import path from 'path'
 
+
+
+
 export default defineConfig({
-  plugins: [vue(),webfontDownload(),],
+  plugins: [vue(),webfontDownload()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './app'),
