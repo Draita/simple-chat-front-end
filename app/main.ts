f5c790dbf7917f443bcc839667dec0b085e9085b
Thijmen <yeaaccount@gmail.com>
2023-03-15 19:43:34 +0100

    added new imports for plugins

--- app/main.ts
+++ app/main.ts
@@ -1,23 +1,34 @@
-import { createApp } from 'vue';
-import App from './App.vue'
-import router from './router';
-import './index.css'
-import '@/axios'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+
+import Croppa from "vue-croppa";
+import "vue-croppa/dist/vue-croppa.css";
+
+import App from "./App.vue";
+
+import router from "./router";
+import "./index.css";
+import "@/axios";
+import vueClickOutsideElement from "vue-click-outside-element";
 
 // Import child components
-import Home from '@/home/Home.vue';
-import ChatApp from '@/chat-app/ChatApp.vue';
+import Home from "@/home/Home.vue";
+import ChatApp from "@/chat-app/ChatApp.vue";
 
-
+const pinia = createPinia();
 
 // Create parent Vue app
 const app = createApp(App);
 // Register router
-app.use(router)
+app.use(router);
 
 // Register child components
-app.component('Home', Home);
-app.component('ChatApp', ChatApp);
+app.component("Home", Home);
+app.component("ChatApp", ChatApp);
+
+app.use(vueClickOutsideElement);
+app.use(pinia);
+app.use(Croppa);
 
 // Mount parent Vue app
-app.mount('#app');
+app.mount("#app");
