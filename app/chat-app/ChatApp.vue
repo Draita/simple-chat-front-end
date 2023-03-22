0e23e53e1e70696c46afc52334f88106a912a2f7
Thijmen <yeaaccount@gmail.com>
2023-03-22 13:12:03 +0100

    added that data now get through sockets //might change this later that this is done through api request because the data is only gotten one time

--- app/chat-app/ChatApp.vue
+++ app/chat-app/ChatApp.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { userStore } from '@/stores/userSelf'
+import { onBeforeUnmount } from 'vue'
 
 
 async function setupData() {
@@ -7,40 +7,81 @@ async function setupData() {
 }
 setupData()
 
-
+onBeforeUnmount(() => {
+  window.socket.$emit('disconnect')
+  window.socket.off("connect")
+  window.socket.off('roomList')
+  window.socket.off('pending friend requests')
+})
 
 </script>
 
 
 <script>
 import { userStore } from '@/stores/userSelf'
+import { menuStore } from '@/stores/menuStorage'
+
+
+
 import APIRequests from "@/APIRequests"
+import io from 'socket.io-client'
+import config from "@/config"
 
 
 export default {
   data() {
     return {
       isLoading: true,
+      isGettingRoomList: true,
+      isGettingFriendList: true,
 
     }
   },
 
   async created() {
-    const userdata = await APIRequests.getUser()
-    const store = userStore()
-    await store.setUserData(userdata.data)
+    const userStorage = userStore();
+    const menuStorage = menuStore();
 
-    this.isLoading = false
 
+
+    const userdata = await APIRequests.getUser();
+    await userStorage.setUserData(userdata.data);
+
+    window.socket = await io(config.apiUrl, {
+      withCredentials: true,
+    });
+    console.log(window.socket);
+
+    // Listen for the "roomList" event on connection
+    window.socket.on("connect", async () => {
+      console.log("Connected to socket");
+      window.socket.emit("getRoomList");
+      window.socket.emit("get pending friend requests");
+
+
+
+    });
+
+    var roomList = []
+    await window.socket.on('roomList', (data) => {
+      // Update the roomList variable
+      menuStorage.setRoomList(data)
+      this.isGettingRoomList = false
+    })
+
+
+    await window.socket.on('pending friend requests', (data) => {
+      // Update the roomList variable
+      menuStorage.setFriendList(data)
+      this.isGettingFriendList = false
+    })
+
+    this.isLoading = false;
   }
-
-
-
 }
-
 </script>
 
 <template>
-  <RouterView v-if = "!isLoading"></RouterView>
+  <RouterView v-if="!isLoading && !isGettingRoomList&& !isGettingFriendList"></RouterView>
 </template>
 
